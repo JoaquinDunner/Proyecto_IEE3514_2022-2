f092c3eeb21880292ef6b8ee988222ba29071326
JoaquinDunner <joaqun.dunner@uc.cl>
2022-11-20 11:21:40 -0300

    e5_nh

--- d2a.m
+++ d2a.m
@@ -1,4 +1,10 @@
 function s_an = d2a(s, Nup, Pulso)
-    s_zerostuffed = upsample(s,Nup);
+    L = length(s);
+    s_zerostuffed = zeros(1,Nup*L);
+    cnt = 1;
+    for i = s
+        s_zerostuffed(cnt) = i;
+        cnt = cnt + Nup;
+    end
     s_an = conv(s_zerostuffed,Pulso,"full");
 end
